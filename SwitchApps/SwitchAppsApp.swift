998165adc65762e483461793aa7fe2a14435f7f7
halu <hlu07472@gmail.com>
2023-06-05 11:24:07 +0900

    Initial Commit

---
+++ SwitchApps/SwitchAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitchAppsApp.swift
+//  SwitchApps
+//
+//  Created by Lu Haoyu on 2023/06/05.
+//
+
+import SwiftUI
+
+@main
+struct SwitchAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
