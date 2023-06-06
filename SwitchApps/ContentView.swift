929eb55e346b8fa48eb24b702fca0332c7ce8257
halu <hlu07472@gmail.com>
2023-06-06 14:11:11 +0900

    edited contents

--- SwitchApps/ContentView.swift
+++ SwitchApps/ContentView.swift
@@ -8,19 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
+    @State var time = 0.0
+    @State var showMessage = true
+    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    
+    var body: some View{
+        content
+            .opacity(showMessage ? 1:0)
+            .scaleEffect(showMessage ? 1:0)
+            .padding(10)
+            .dynamicTypeSize(.xSmall ... .xxLarge)
+    }
+    
+    var content: some View {
+        VStack(alignment: .center, spacing: 20.0) {
+            Image(systemName: "timelapse", variableValue: time)
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                .font(.system(size: 50))
+                .fontWeight(.thin)
+                .onReceive(timer) { value in
+                    if time < 1.0{
+                        time += 0.1
+                        
+                    }else{
+                        time = 0.0
+                    }
+                }
+            Text("Switching Apps".uppercased())
+                .font(.largeTitle.width(.condensed))
+                .fontWeight(.bold)
+            Text("Tap and hold any part of the screen for 1 second to show the menu for switching between apps")
+                .multilineTextAlignment(.center)
+                .foregroundColor(.secondary)
+                .fontWeight(.medium)
+            Button{
+                withAnimation(.easeInOut) {
+                    showMessage = false
+                }
+            }label: {
+                Text("Got it")
+                    .padding(.all)
+                    .frame(maxWidth: .infinity)
+                    .background(.white.opacity(0.2).gradient)
+                    .cornerRadius(10)
+                    .overlay(stroke)
+            }
+            .accentColor(.primary)
+            .shadow(radius: 10)
+                
+                
         }
-        .padding()
+        .padding(30)
+        .background(.ultraThinMaterial)
+        .cornerRadius(10)
+        .overlay(stroke)
+        .frame(maxWidth: 500)
+        .shadow(color:.black.opacity(0.3), radius: 10,y:10)
+    }
+    
+    var stroke: some View{
+        RoundedRectangle(cornerRadius: 10)
+            .stroke()
+            .foregroundStyle(.linearGradient(colors: [.white.opacity(0.5), .clear,
+                .white.opacity(0.5), .clear], startPoint: .topLeading, endPoint: .bottomTrailing))
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .background(Image("Wallpaper 2"))
     }
 }
